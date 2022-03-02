42e6840b5751653bec3141af1612f29e1abdf917
turtle601 <turtle601@naver.com>
2022-03-02 16:05:44 +0900

    chores : DB 연결 테스트 코드 #1

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,36 @@
+require('dotenv').config();
+
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const { PORT, MONGO_URI } = process.env;
+
+const port = PORT || 4000;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(e => {
+    console.error(e);
+  });
 
 const app = new Koa();
+const router = new Router();
 
 app.use(ctx => {
   ctx.body = 'hello world';
 });
 
-app.listen(4000, () => {
+app.use(bodyParser());
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () => {
   console.log('Listening to http://localhost:4000');
 });
