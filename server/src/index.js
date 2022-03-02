5b13c476a39e6fc1062bb728f5cbd22bfb8462c3
turtle601 <turtle601@naver.com>
2022-03-02 16:17:09 +0900

    chores : esm 모듈 다운로드 및 셋팅 #1

--- server/src/index.js
+++ server/src/index.js
@@ -1,36 +1,4 @@
-require('dotenv').config();
+// eslint-disable-next-line no-global-assign
+require = require('esm')(module);
 
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
-
-const { PORT, MONGO_URI } = process.env;
-
-const port = PORT || 4000;
-
-mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(e => {
-    console.error(e);
-  });
-
-const app = new Koa();
-const router = new Router();
-
-app.use(ctx => {
-  ctx.body = 'hello world';
-});
-
-app.use(bodyParser());
-
-app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(port, () => {
-  console.log('Listening to http://localhost:4000');
-});
+module.exports = require('./main.js');
