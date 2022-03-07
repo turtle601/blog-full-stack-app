9ef500dc2668b4ea6fdf0440db9eb438e460a37f
turtle601 <turtle601@naver.com>
2022-03-07 13:48:08 +0900

    feat: User 스키마/인스턴스 메서드 생성 #7 비밀번호 설정 후 hashedPassword 만들기, hashedPassword와 비밀번호 비교 메서드 생성

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const { Schema } = mongoose;
 
@@ -6,6 +7,20 @@ const UserSchema = new Schema({
   username: String,
   hashedPassword: String,
 });
+
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result;
+};
+
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({ username });
+};
 
 const User = mongoose.model('User', UserSchema);
 
