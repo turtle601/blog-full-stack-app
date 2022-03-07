a6e7cfc8720fd578128d57414455f97e1ce8a42f
turtle601 <turtle601@naver.com>
2022-03-07 09:34:33 +0900

    feat: post 전체 내용 읽기 구현 #6

--- server/src/api/posts/post.ctrl.js
+++ server/src/api/posts/post.ctrl.js
@@ -1,18 +1,31 @@
 import Post from '../../models/users';
 
-export const list = ctx => {
-  ctx.body = 'list';
+// GET api/posts
+export const list = async ctx => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
-export const write = ctx => {
+// POST api/posts
+export const write = async ctx => {
   ctx.body = 'write';
 };
+
+// GET api/posts/{postId}
 export const read = ctx => {
   ctx.body = 'read';
 };
+
+// DELETE api/posts/{postId}
 export const remove = ctx => {
   ctx.body = 'remove';
 };
+
+// PATCH api/posts/{postid}
 export const patch = ctx => {
   ctx.body = 'patch';
 };
