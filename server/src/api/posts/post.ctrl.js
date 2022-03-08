f9cb8527af577f5eafa8bb62a6d750f2eb5795dc
turtle601 <turtle601@naver.com>
2022-03-08 10:12:30 +0900

    feat : Post Api 페이지네이션 구현 #10

--- server/src/api/posts/post.ctrl.js
+++ server/src/api/posts/post.ctrl.js
@@ -18,9 +18,23 @@ export const checkPostId = (ctx, next) => {
 
 // GET api/posts
 export const list = async ctx => {
+  const page = parseInt(ctx.query.page || '1', 10);
+
   try {
-    const posts = await Post.find().exec();
-    ctx.body = posts;
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean()
+      .exec();
+
+    const postCount = await Post.countDocuments().exec();
+    ctx.set('Last-page', Math.ceil(postCount / 10));
+    ctx.body = posts.map(post => ({
+      ...post,
+      body:
+        post.body.length > 200 ? `${post.body.slice(0, 200)}...` : post.body,
+    }));
   } catch (e) {
     ctx.throw(500, e);
   }
