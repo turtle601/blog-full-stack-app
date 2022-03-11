9ec3fc4216b0c1069eb7ab29f461aac8ee3689b0
turtle601 <turtle601@naver.com>
2022-03-11 14:05:14 +0900

    feat: input 커스터마이징 #22

--- client/src/components/auth/AuthForm.jsx
+++ client/src/components/auth/AuthForm.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 // custom 컴포넌트
 import { Button } from '../../customs/button';
+import { Input } from '../../customs/input';
 
 const AuthForm = () => {
   return (
-    <>
-      <div>AuthForm</div>
+    <form>
+      <Input underline />
+      <Input underline />
       <Button fullWidth cyan>
         회원가입
       </Button>
-    </>
+    </form>
   );
 };
 
