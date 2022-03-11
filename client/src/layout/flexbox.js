e1c777bb8624b26a1ff6e58f5d40bd87f71b589f
turtle601 <turtle601@naver.com>
2022-03-11 10:39:45 +0900

    feat : 기본적인 flexbox, gridbox의 스타일 컴포넌트 생성 #22

---
+++ client/src/layout/flexbox.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Flex = styled.div`
+  display: flex;
+`;
+
+export const Center = styled(Flex)`
+  justify-content: center;
+  align-items: center;
+`;
