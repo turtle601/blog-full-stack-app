bed0379c3cc538991b8dcbdf362d00a71793b119
turtle601 <turtle601@naver.com>
2022-03-11 19:36:19 +0900

    feat: authReducer 작성 #26

---
+++ client/src/stores/auth.js
@@ -0,0 +1,51 @@
+// immutable 라이브러리
+import { Map } from 'immutable';
+
+// redux-actions 라이브러리
+import { createAction, handleActions } from 'redux-actions';
+
+// types
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+// actions
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }) => ({
+    form, // register, login
+    key, // username, password, passwordConfirm
+    value, // 입력값
+  }),
+);
+
+// 결과값 : register or login
+export const initializeForm = createAction(INITIALIZE_FORM, form => form);
+
+// state
+const initialState = Map({
+  register: Map({
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  }),
+  login: Map({
+    username: '',
+    password: '',
+  }),
+});
+
+// reducer
+const authReducer = handleActions(
+  {
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) => {
+      state.set([form, key], value);
+    },
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+    }),
+  },
+  initialState,
+);
+
+export default authReducer;
--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+
+// reducer 가져오기
 import darkReducer from './dark';
+import authReducer from './auth';
 
 const rootReducer = combineReducers({
   darkReducer,
+  authReducer,
 });
 
 export default rootReducer;
