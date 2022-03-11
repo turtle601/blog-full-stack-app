5e7ee49806c4fd990372320115ed9ca309a1c175
turtle601 <turtle601@naver.com>
2022-03-11 12:57:36 +0900

    feat: button 커스터마이징 #22

--- client/src/components/auth/AuthForm.jsx
+++ client/src/components/auth/AuthForm.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+// custom 컴포넌트
+import { Button } from '../../customs/button';
+
 const AuthForm = () => {
-  return <div>AuthForm</div>;
+  return (
+    <>
+      <div>AuthForm</div>
+      <Button fullWidth cyan>
+        회원가입
+      </Button>
+    </>
+  );
 };
 
 export default AuthForm;
