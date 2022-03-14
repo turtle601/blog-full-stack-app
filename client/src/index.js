2551d6b6780acb3ffa6a95cdc19bbcba6d20c0d6
turtle601 <turtle601@naver.com>
2022-03-14 12:11:28 +0900

    chores : 스토어에 redux-saga 미들웨어 적용 #30

--- client/src/index.js
+++ client/src/index.js
@@ -7,13 +7,30 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 // redux 관련 라이브러리
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
+// redux-saga 미들웨어
+import createSagaMiddleware from 'redux-saga';
+
+// redux-logger 라이브러리
+import { createLogger } from 'redux-logger';
+
 import rootReducer from './stores';
+import rootSaga from './sagas';
 
 import App from './App';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const logger = createLogger();
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, sagaMiddleware)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Router>
