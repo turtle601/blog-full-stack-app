85e8be6b80b68ff492ff16ccb3217ed12711b449
turtle601 <turtle601@naver.com>
2022-03-14 13:38:43 +0900

    feat : loading 리듀서 셋팅 #30

--- client/src/stores/index.js
+++ client/src/stores/index.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 // reducer 가져오기
 import darkReducer from './dark';
 import authReducer from './auth';
+import loadingReducer from './loading';
 
 const rootReducer = combineReducers({
   darkReducer,
   authReducer,
+  loadingReducer,
 });
 
 export default rootReducer;
---
+++ client/src/stores/loading.js
@@ -0,0 +1,38 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const START_LOADING = 'loading/START_LOADING';
+const FINISH_LOADING = 'loading/FINISH_LOADING';
+
+export const startLoading = createAction(
+  START_LOADING,
+  requestType => requestType,
+);
+
+export const finishLoading = createAction(
+  FINISH_LOADING,
+  requestType => requestType,
+);
+
+const initialState = {};
+
+const loadingReducer = handleActions(
+  {
+    [START_LOADING]: (state, action) => {
+      console.log('loadingReducer에 대한 액션' + action); //
+      return {
+        ...state,
+        [action.payload]: true,
+      };
+    },
+
+    [FINISH_LOADING]: (state, action) => {
+      return {
+        ...state,
+        [action.payload]: false,
+      };
+    },
+  },
+  initialState,
+);
+
+export default loadingReducer;
